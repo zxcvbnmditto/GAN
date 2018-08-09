726165902d4b2722c58c36ea2563d769a35ab565
Chu Hung Cheng <chccheng@ucdavis.edu>
2018-08-10 01:04:56 +0800

    Fix wgan-gp

---
+++ wgan-gp/utils.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import glob
+import os
+import tensorflow as tf
+
+from skimage import io
+
+data_dir = '../faces/64-64/'
+
+class DataLoader:
+    def __init__(self, batch_size):
+        self.images = self.load_data()
+        self.batch_iter = -1
+        self.batch_size = batch_size
+        self.batches = self.create_new_batches()
+
+    def load_data(self):
+        print("LOADING IMAGES ........")
+        files = sorted(glob.glob(data_dir + '*.jpg'))
+
+        images = []
+        for file in files:
+            image = io.imread(file)
+            image = np.array(image)
+            images.append(image)
+
+        # [batch, 96, 96, 3]
+        images = np.array(images)
+        outputs = (images / 127.5) - 1
+        return outputs
+
+    def create_new_batches(self):
+        print("CREATING NEW BATCHES ........")
+        np.random.shuffle(self.images)
+
+        batches = []
+        for i in range(0, len(self.images), self.batch_size):
+            if len(self.images) < i + self.batch_size:
+                break
+            batches.append(self.images[i: i + self.batch_size])
+
+        batches = np.array(batches)
+        return np.array(batches)
+
+    def get_nextbatch(self):
+        if self.batches.shape[0] <= self.batch_iter + 1:
+            self.batches = self.create_new_batches()
+            self.batch_iter = -1
+
+        self.batch_iter += 1
+        return self.batches[self.batch_iter]
+
+
+def to_range(images, max, min, type):
+    return ((images + 1.) / 2. * (max - min) + min).astype(type)
+
+
+def immerge_save(images, epoch):
+    images = np.array(images).squeeze()
+
+    row, col = 8, 8
+    h, w, c = images[1], images[2], images[3]
+    path_dir = 'sample_img/'
+
+    if not os.path.isdir(path_dir):
+        os.makedirs(path_dir)
+
+    path_dir = path_dir + str(epoch) + '.jpg'
+    img = np.zeros((h * row, w * col, c))
+
+    for idx, image in enumerate(images):
+        i = idx % col
+        j = idx // col
+        img[j * h:j * h + h, i * w:i * w + w, ...] = image
+
+    img = to_range(img, 0, 255, np.uint8)
+
+    return io.imsave(path_dir, img)
--- wgan-gp/wgan_gp.py
+++ wgan-gp/wgan_gp.py
@@ -9,13 +9,13 @@ class WGAN_GP():
         self.Lambda = 10
         self.X = tf.placeholder(tf.float32, shape=[None, 64, 64, 3])
         self.Z = tf.placeholder(tf.float32, shape=[None, self.latent_size])
-
+        self.training = tf.placeholder(tf.bool)
         self.build()
 
     # filter shape = [filter_width, filter_height, channel (prev filter number), output_filter_num]
     # stride shape = [1, width_stride, height_stride, 1]
     def conv2d(self, x, w):
-        return tf.nn.conv2d(input=x, filter=w, strides=[1, 1, 1, 1], padding='SAME')
+        return tf.nn.conv2d(input=x, filter=w, strides=[1, 2, 2, 1], padding='SAME')
 
     def deconv2d(self, x, w, shape):
         return tf.nn.conv2d_transpose(value=x, filter=w, output_shape=shape, strides=[1, 2, 2, 1], padding='SAME')
@@ -34,33 +34,40 @@ class WGAN_GP():
             #  64 - 32  - 16 -  8 - 4
 
             # Conv layer 1
-            w1 = tf.get_variable('d_w1', [4, 4, 3, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
-            conv_1 = tf.nn.leaky_relu(self.conv2d(x, w1) + b1)
-            pool_1 = self.pooling(conv_1)
+            w1 = tf.get_variable('d_w1', [4, 4, 3, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
+            b1 = tf.get_variable('d_b1', [64], initializer=tf.constant_initializer(0))
+            conv_1 = tf.nn.leaky_relu(self.conv2d(x, w1) + b1, alpha=0.2)
+            conv_1 = tf.contrib.layers.layer_norm(conv_1, scope='d_ln1')
+
+            # pool_1 = self.pooling(conv_1)
 
             # Conv layer 2
-            w2 = tf.get_variable('d_w2', [4, 4, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b2 = tf.get_variable('d_b2', [64], initializer=tf.constant_initializer(0))
-            conv_2 = tf.nn.leaky_relu(self.conv2d(pool_1, w2) + b2)
-            pool_2 = self.pooling(conv_2)
+            w2 = tf.get_variable('d_w2', [4, 4, 64, 128], initializer=tf.truncated_normal_initializer(stddev=0.02))
+            b2 = tf.get_variable('d_b2', [128], initializer=tf.constant_initializer(0))
+            conv_2 = tf.nn.leaky_relu(self.conv2d(conv_1, w2) + b2, alpha=0.2)
+            conv_2 = tf.contrib.layers.layer_norm(conv_2, scope='d_ln2')
+
+            # pool_2 = self.pooling(conv_2)
 
             # Conv layer 3
-            w3 = tf.get_variable('d_w3', [4, 4, 64, 128], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b3 = tf.get_variable('d_b3', [128], initializer=tf.constant_initializer(0))
-            conv_3 = tf.nn.leaky_relu(self.conv2d(pool_2, w3) + b3)
-            pool_3 = self.pooling(conv_3)
+            w3 = tf.get_variable('d_w3', [4, 4, 128, 256], initializer=tf.truncated_normal_initializer(stddev=0.02))
+            b3 = tf.get_variable('d_b3', [256], initializer=tf.constant_initializer(0))
+            conv_3 = tf.nn.leaky_relu(self.conv2d(conv_2, w3) + b3, alpha=0.2)
+            conv_3 = tf.contrib.layers.layer_norm(conv_3, scope='d_ln3')
+
+            # pool_3 = self.pooling(conv_3)
 
             # Conv layer 4
-            w4 = tf.get_variable('d_w4', [4, 4, 128, 256], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b4 = tf.get_variable('d_b4', [256], initializer=tf.constant_initializer(0))
-            conv_4 = tf.nn.leaky_relu(self.conv2d(pool_3, w4) + b4)
-            pool_4 = self.pooling(conv_4)
+            w4 = tf.get_variable('d_w4', [4, 4, 256, 512], initializer=tf.truncated_normal_initializer(stddev=0.02))
+            b4 = tf.get_variable('d_b4', [512], initializer=tf.constant_initializer(0))
+            conv_4 = tf.nn.leaky_relu(self.conv2d(conv_3, w4) + b4, alpha=0.2)
+            conv_4 = tf.contrib.layers.layer_norm(conv_4, scope='d_ln4')
+            # pool_4 = self.pooling(conv_4)
 
-            flatten = tf.layers.flatten(pool_4)
+            flatten = tf.reshape(conv_4, [self.batch_size, 4 * 4 * 512])
 
             # Dense layer 1
-            w5 = tf.get_variable('d_w5', [4 * 4 * 256, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+            w5 = tf.get_variable('d_w5', [4 * 4 * 512, 1], initializer=tf.random_normal_initializer(stddev=0.02))
             b5 = tf.get_variable('d_b5', [1], initializer=tf.constant_initializer(0))
             dense1 = tf.matmul(flatten, w5) + b5
 
@@ -68,60 +75,71 @@ class WGAN_GP():
 
         return outputs
 
-    def generator(self, x, reuse=False):
+    def generator(self, x, reuse=False, training=True):
         print("Generator ...........")
         with tf.variable_scope('generator') as scope:
             if (reuse):
                 scope.reuse_variables()
 
             # Dense layer 1
-            w1 = tf.get_variable('g_w1', [self.latent_size, 128 * 16 * 16], initializer=tf.truncated_normal_initializer(
+            w1 = tf.get_variable('g_w1', [self.latent_size, 512 * 4 * 4], initializer=tf.random_normal_initializer(
                 stddev=0.02))
-            b1 = tf.get_variable('g_b1', [128 * 16 * 16], initializer=tf.constant_initializer(0))
-
+            b1 = tf.get_variable('g_b1', [512 * 4 * 4], initializer=tf.constant_initializer(0))
             dense1 = tf.nn.relu(tf.matmul(x, w1) + b1)
-            dense1 = tf.reshape(dense1, [self.batch_size, 16, 16, 128])
-
+            dense1 = tf.reshape(dense1, [self.batch_size, 4, 4, 512])
+            dense1 = tf.contrib.layers.batch_norm(dense1, scope='g_bn1', decay=0.9, epsilon=1e-5, scale=True,
+                                                  is_training=training)
             # 256 - 128 - 64 - 32 - 3
             #  64 - 32  - 16 -  8 - 4
 
             # Deconv layer 1
-            o_shape1 = [self.batch_size, 32, 32, 128]
-            w2 = tf.get_variable('g_w2', [4, 4, 128, 128], initializer=tf.truncated_normal_initializer(
+            o_shape1 = [self.batch_size, 8, 8, 256]
+            w2 = tf.get_variable('g_w2', [4, 4, 256, 512], initializer=tf.random_normal_initializer(
                 stddev=0.01))
-            b2 = tf.get_variable('g_b2', [128], initializer=tf.constant_initializer(0.1))
+            b2 = tf.get_variable('g_b2', [256], initializer=tf.constant_initializer(0.1))
             deconv1 = self.deconv2d(dense1, w2, o_shape1) + b2
-            deconv1 = tf.contrib.layers.batch_norm(inputs=deconv1, decay=0.9, is_training=True,
-                                                   scope="g_bn1")
-
             deconv1 = tf.nn.relu(deconv1)
+            deconv1 = tf.contrib.layers.batch_norm(deconv1, scope='g_bn2', decay=0.9, epsilon=1e-5, scale=True,
+                                                   is_training=training)
+
 
             # Deconv layer 2
-            o_shape2 = [self.batch_size, 64, 64, 64]
-            w3 = tf.get_variable('g_w3', [4, 4, 64, 128], initializer=tf.truncated_normal_initializer(
+            o_shape2 = [self.batch_size, 16, 16, 128]
+            w3 = tf.get_variable('g_w3', [4, 4, 128, 256], initializer=tf.random_normal_initializer(
                 stddev=0.01))
-            b3 = tf.get_variable('g_b3', [64], initializer=tf.constant_initializer(0.1))
+            b3 = tf.get_variable('g_b3', [128], initializer=tf.constant_initializer(0.1))
             deconv2 = self.deconv2d(deconv1, w3, o_shape2) + b3
-            deconv2 = tf.contrib.layers.batch_norm(inputs=deconv2, decay=0.9, is_training=True,
-                                                   scope="g_bn2")
-
             deconv2 = tf.nn.relu(deconv2)
+            deconv2 = tf.contrib.layers.batch_norm(deconv2, scope='g_bn3', decay=0.9, epsilon=1e-5, scale=True,
+                                                   is_training=training)
+
 
             # Deconv layer 3
-            # o_shape3 = [self.batch_size, 64, 64, 3]
-            w4 = tf.get_variable('g_w4', [4, 4, 64, 3], initializer=tf.truncated_normal_initializer(
+            o_shape3 = [self.batch_size, 32, 32, 64]
+            w4 = tf.get_variable('g_w4', [4, 4, 64, 128], initializer=tf.random_normal_initializer(
                 stddev=0.01))
-            b4 = tf.get_variable('g_b4', [3], initializer=tf.constant_initializer(0.1))
-            deconv3 = self.conv2d(deconv2, w4) + b4
+            b4 = tf.get_variable('g_b4', [64], initializer=tf.constant_initializer(0.1))
+            deconv3 = self.deconv2d(deconv2, w4, o_shape3) + b4
+            deconv3 = tf.nn.relu(deconv3)
+            deconv3 = tf.contrib.layers.batch_norm(deconv3, scope='g_bn4', decay=0.9, epsilon=1e-5, scale=True,
+                                                   is_training=training)
 
-            outputs = tf.nn.tanh(deconv3)
+
+            # Deconv layer 4
+            o_shape4 = [self.batch_size, 64, 64, 3]
+            w5 = tf.get_variable('g_w5', [4, 4, 3, 64], initializer=tf.random_normal_initializer(
+                stddev=0.01))
+            b5 = tf.get_variable('g_b5', [3], initializer=tf.constant_initializer(0.1))
+            deconv4 = self.deconv2d(deconv3, w5, o_shape4) + b5
+
+            outputs = tf.nn.tanh(deconv4)
 
         return outputs
 
     def build(self):
         # real => images from database
         self.Dx = self.discriminator(self.X, reuse=False)
-        self.Gz = self.generator(self.Z, reuse=False)
+        self.Gz = self.generator(self.Z, reuse=False, training=self.training)
         # fake => images generated by gaussian distribution
         self.DG = self.discriminator(self.Gz, reuse=True)
 
@@ -140,14 +158,21 @@ class WGAN_GP():
         interpolates = self.X + penalty_dist * differences
         grads = tf.gradients(self.discriminator(interpolates, reuse=True), [interpolates])[0]
         slopes = tf.sqrt(tf.reduce_sum(tf.square(grads), reduction_indices=[1]))
-        gradient_penalty = tf.reduce_mean((slopes - 1) ** 2)
+        self.gradient_penalty = tf.reduce_mean((slopes - 1) ** 2)
 
         # GRADIENT PENALTY can be viewed as a sort of regularization
 
-        self.d_loss = self.d_fake - self.d_real + self.Lambda * gradient_penalty
+        self.d_loss = self.d_fake - self.d_real + self.Lambda * self.gradient_penalty
 
-        d_vars = [var for var in tf.trainable_variables() if 'd_' in var.name]
-        g_vars = [var for var in tf.trainable_variables() if 'g_' in var.name]
+        self.d_sumop = tf.summary.scalar('d_loss', self.d_loss)
+        self.g_sumop = tf.summary.scalar('g_loss', self.g_loss)
+        self.p_sumop = tf.summary.scalar('penalty', self.gradient_penalty)
+
+        d_vars = [var for var in tf.trainable_variables() if 'discriminator' in var.name]
+        g_vars = [var for var in tf.trainable_variables() if 'generator' in var.name]
+
+        print(d_vars)
+        print(g_vars)
 
         with tf.variable_scope(tf.get_variable_scope(), reuse=False) as scope:
             print("reuse or not: {}".format(tf.get_variable_scope().reuse))
@@ -159,5 +184,5 @@ class WGAN_GP():
             self.trainerG = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.5, beta2=0.999).minimize(
                 self.g_loss, var_list=g_vars)
 
-        self.saver = tf.train.Saver(tf.global_variables())
+        self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=3)
 
